296435cf01855e08004faaab896c64748d5b3004
barth-Mboungou <barthmboungou@gmail.com>
2024-09-20 10:24:33 +0200

    research and pagination

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,7 @@
 import SideNav from "../ui/dashboard/sidenav";
 import Page from "./(overview)/page";
 
+export const experimental_ppr = true;
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
